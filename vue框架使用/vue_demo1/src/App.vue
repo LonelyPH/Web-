89cb943ea49bf4f781a85495658b599e896ea525
LonelyPH <752325363@qq.com>
2019-03-28 17:19:34 +0800

    修改

--- vue框架使用/vue_demo1/src/App.vue
+++ vue框架使用/vue_demo1/src/App.vue
@@ -3,8 +3,8 @@
     <div>
       <h1>Router Basic -01</h1>
     </div>
-    <router-link to="/about">About</router-link>
-    <router-link to="/home">Home</router-link>
+    <router-link to="/about" class="list-group-item">About</router-link>
+    <router-link to="/home" class="list-group-item">Home</router-link>
 
     <div>
       <router-view></router-view>
